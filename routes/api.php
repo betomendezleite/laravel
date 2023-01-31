9ead44bb3dd73d5fc655809cf07fbf3046968e09
betomendezleite <bmlaudiovisuales@gmail.com>
2023-01-31 01:10:18 -0400

    commit-v2

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\PlanesController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CapitulosController;
+use App\Http\Controllers\PeliculasController;
+use App\Http\Controllers\SeriesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +19,53 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+
+
+Route::post('login', [AuthController::class, 'login']);
+
+Route::get('planes', [PlanesController::class, 'index']);
+
+
+
+Route::middleware('auth:sanctum')->group(function () {
+
+
+    Route::get('usuarios', [AuthController::class, 'index']);
+    Route::post('register', [AuthController::class, 'register']);
+
+    Route::post('usuarios-update', [AuthController::class, 'update']);
+    Route::post('usuarios-delete', [AuthController::class, 'destroy']);
+
+
+    Route::post('planes-create', [PlanesController::class, 'create']);
+    Route::post('planes-update', [PlanesController::class, 'update']);
+    Route::post('planes-delete', [PlanesController::class, 'destroy']);
+
+    Route::post('logouts', [AuthController::class, 'logouts']);
+
+    //PELICULAS
+    Route::get('peliculas', [PeliculasController::class, 'index']);
+    Route::post('peliculas-create', [PeliculasController::class, 'create']);
+    Route::post('peliculas-update', [PeliculasController::class, 'update']);
+    Route::post('peliculas-delete', [PeliculasController::class, 'delete']);
+
+
+    //SERIES
+    Route::get('series', [SeriesController::class, 'index']);
+    Route::post('series-create', [SeriesController::class, 'create']);
+    Route::post('series-update', [SeriesController::class, 'update']);
+    Route::post('series-delete', [SeriesController::class, 'delete']);
+
+    //CAPITULOS
+    Route::get('capitulos', [CapitulosController::class, 'index']);
+    Route::post('capitulos-create', [CapitulosController::class, 'create']);
+    Route::post('capitulos-update', [CapitulosController::class, 'update']);
+    Route::post('capitulos-delete', [CapitulosController::class, 'delete']);
+
+    //FAVORITOS SERIES
+
+
+
+
 });
