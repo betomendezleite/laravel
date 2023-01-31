c5decc0e85dea7e72d0e34b9189f2deb1f3ead62
betomendezleite <bmlaudiovisuales@gmail.com>
2023-01-31 00:41:17 -0400

    commit-v1

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+use App\Models\Planes;
+use \stdClass;
+
+class AuthController extends Controller
+{
+
+    public function index()
+    {
+        $user = User::all();
+        return $user;
+    }
+
+    public function register(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'nombres' => 'required|string|max:255',
+            'apellidos' => 'required|string|max:255',
+            'username' => 'required|string|max:255|unique:users',
+            'admin' => 'required|string|max:255',
+            'telefonos' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+            'planes_id' => 'required|string|max:255',
+            'vencimiento_plan' => 'required|string|max:255',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        $user = User::create([
+            'nombres' => $request->nombres,
+            'apellidos' => $request->apellidos,
+            'username' => $request->username,
+            'admin' => $request->admin,
+            'telefonos' => $request->telefonos,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'planes_id' => $request->planes_id,
+            'vencimiento_plan' => $request->vencimiento_plan,
+        ]);
+
+
+        return response()->json([
+            'message' => 'El usuario: ' . $request->username . ' ha sido creado con exito',
+        ]);
+    }
+    public function login(Request $request)
+    {
+
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'NO AUTORIZADO'
+            ], 401);
+        }
+
+
+
+        $user = User::where('email', $request->email)->firstOrFail();
+
+        $verificar = DB::table('personal_access_tokens')->where('tokenable_id', $user->id)->get();
+        $verificar = $verificar->count() + 1;
+        $usrinfo = User::join('planes', 'planes.id', '=', 'users.id')
+            ->select('*')
+            ->where('email', $request->email)
+            ->get();
+
+
+        if ($verificar > $usrinfo[0]['dispositivos']) {
+
+            return response()->json([
+                'message' => 'Numero de dispositivos ha superado el limite, deseas eliminar las sesiones?'
+            ]);
+        } else {
+            $token = $user->createtoken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'message' => 'Hola ' . $user->name,
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+                'user' => $user,
+                'dispositivos' => $verificar
+            ]);
+        }
+    }
+
+    public function logouts()
+    {
+        auth()->user()->tokens()->delete();
+        return [
+            'message' => 'Mensaje: Tokens eliminados'
+        ];
+    }
+
+    public function update(Request $request, user $users)
+    {
+        /*         $request->validate([
+            'nombres' => 'required|string|max:255',
+            'apellidos' => 'required|string|max:255',
+            'username' => 'required|string|max:255|unique:users',
+            'admin' => 'required|string|max:255',
+            'telefonos' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+            'planes_id' => 'required|string|max:255',
+            'vencimiento_plan' => 'required|string|max:255',
+        ]);
+ */
+        $users = User::find($request->id);
+        $users->nombres = $request->nombres;
+        $users->apellidos = $request->apellidos;
+        $users->username = $request->username;
+        $users->admin = $request->admin;
+        $users->telefonos = $request->telefonos;
+        $users->email = $request->email;
+        $users->password = Hash::make($request->password);
+        $users->planes_id = $request->planes_id;
+        $users->vencimiento_plan = $request->vencimiento_plan;
+
+        $users->save();
+
+        return response()->json([
+            'message' => 'Usuario: ' . $users->username . ' actualizado con exito'
+        ]);
+    }
+    public function destroy(Request $request)
+    {
+        $request->validate([
+            'id' => 'required|string|max:255',
+        ]);
+
+        $planes = User::destroy($request->id);
+
+
+        return response()->json([
+            'message' => 'Usuario eliminado con exito'
+        ]);
+    }
+}
---
+++ app/Http/Controllers/Favoritos_PeliculasController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Favoritos_seriess;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class Favoritos_PeliculasController extends Controller
+{
+    public function index()
+    {
+        return Favoritos_seriess::all();
+    }
+    public function create(Request $request)
+    {
+        if (Auth::user()->admin === '0') {
+            return res_mensaje('No tienes privilegios para realizar esta acción');
+        } else {
+
+
+            $request->validate([
+                'id_thmdb' => 'required|string|max:255',
+                'img_thumb' => 'required|string|max:255',
+                'img_portada' => 'required|string|max:255',
+                'url_1080' => 'required|string|max:255',
+                'url_720' => 'required|string|max:255',
+                'url_480' => 'required|string|max:255'
+            ]);
+
+
+            $pel = Peliculas::create([
+                'id_thmdb' => $request->id_thmdb,
+                'img_thumb' => $request->img_thumb,
+                'img_portada' => $request->img_portada,
+                'url_1080' => $request->url_1080,
+                'url_720' => $request->url_720,
+                'url_480' => $request->url_480
+
+            ]);
+
+            return res_mensaje('Pelicula registrada con exito');
+        }
+    }
+    public function update(Request $request, peliculas $peliculas)
+    {
+
+
+        if (Auth::user()->admin === '0') {
+            return res_mensaje('No tienes privilegios para realizar esta acción');
+        } else {
+
+
+            $request->validate([
+                'id_thmdb' => 'required|string|max:255',
+                'img_thumb' => 'required|string|max:255',
+                'img_portada' => 'required|string|max:255',
+                'url_1080' => 'required|string|max:255',
+                'url_720' => 'required|string|max:255',
+                'url_480' => 'required|string|max:255'
+            ]);
+
+
+            $peliculas = Peliculas::find($request->id);
+            $peliculas->id_thmdb = $request->id_thmdb;
+            $peliculas->img_thumb = $request->img_thumb;
+            $peliculas->img_portada = $request->img_portada;
+            $peliculas->url_1080 = $request->url_1080;
+            $peliculas->url_720 = $request->url_720;
+            $peliculas->url_480 = $request->url_480;
+
+            $peliculas->save();
+
+            return res_mensaje('Pelicula actualizada con exito');
+        }
+    }
+    public function delete(Request $request)
+    {
+
+        if (Auth::user()->admin === '0') {
+            return res_mensaje('No tienes privilegios para realizar esta acción');
+        } else {
+
+            $request->validate([
+                'id' => 'required|string|max:255',
+            ]);
+
+            $planes = Peliculas::destroy($request->id);
+
+
+            return res_mensaje('Pelicula eliminada con exito');
+        }
+    } 
+}
